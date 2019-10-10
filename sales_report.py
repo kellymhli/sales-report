f9da103b472b32b4081a1ac698fdc4d29410499d
kellymhli <55807683+kellymhli@users.noreply.github.com>
2019-10-09 19:39:31 -0700

    Refactor function to use a dictionary rather than 2 lists.

---
+++ sales_report.py
@@ -0,0 +1,57 @@
+def print_using_lists(filename):
+    """Take in a file object and print sales report."""
+
+    # Generate two lists 
+    salespeople = []
+    melons_sold = []
+
+    # Parse out the salesperson and number of melons sold from each line
+    for line in filename:
+        line = line.rstrip()
+        entries = line.split('|')
+        salesperson = entries[0]
+        melons = int(entries[2])
+
+        # Add the salesperson's name to one list at a specific index
+        # At the same index in melons_sold, log the total melons sold 
+        if salesperson in salespeople:
+            position = salespeople.index(salesperson)
+            melons_sold[position] += melons
+        else:
+            salespeople.append(salesperson)
+            melons_sold.append(melons)
+
+    # Print salesperson and num of melons they sold
+    for i in range(len(salespeople)):
+        print(f'{salespeople[i]} sold {melons_sold[i]} melons')
+
+
+def print_using_dict(filename):
+    """Take in a file object and print sales report."""
+
+    sales_data = {}
+
+    # Parse out the salesperson and number or melons sold from each line
+    for line in filename:
+        line = line.rstrip()
+        entries = line.split('|')
+        salesperson = entries[0]
+        melons = int(entries[2])
+
+        # Add salesperson to and increment melons sold in dictionary
+        sales_data[salesperson] = sales_data.get(salesperson, 0)
+        sales_data[salesperson] += melons
+
+    # Print salesperson and num of melons they sold
+    for person, total_melons in sales_data.items():
+        print(f'{person} sold {total_melons} melons')
+
+
+if __name__ == '__main__':
+
+    # Open file and pass into print_using_dict function
+    f = open('sales-report.txt')
+    print_using_dict(f)
+
+
+
